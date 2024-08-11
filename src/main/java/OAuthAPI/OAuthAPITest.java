c695d27e0f0626b0e67b88ae25ddfdfd665136a5
Pratap <pratapbhowmick29@gmail.com>
2024-08-11 16:52:28 +0530

    POJO class created

--- src/main/java/OAuthAPI/OAuthAPITest.java
+++ src/main/java/OAuthAPI/OAuthAPITest.java
@@ -7,7 +7,12 @@ import static io.restassured.RestAssured.*;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import POJO.Api;
+import POJO.GetCourse;
 import Utility.PropertyReader;
 public class OAuthAPITest {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
@@ -34,16 +39,41 @@ public class OAuthAPITest {
 		String oAuthToken=js1.getString("access_token");
 		System.out.println("OAuth token granted : "+oAuthToken);
 		
-		//Get Call using OAuth token
-		String getResponse=given()
-			.queryParam("access_token", oAuthToken)
-		.get("oauthapi/getCourseDetails")
-		.then()
-			.log().all()
-			.extract().response().asString();
-		JsonPath js2=new JsonPath(getResponse);
-		String courses=js2.getString("courses");
-		System.out.println("Fetched courses are : "+courses);
+//		//Get Call using OAuth token
+//		String getResponse=given()
+//			.queryParam("access_token", oAuthToken)
+//		.get("oauthapi/getCourseDetails")
+//		.then()
+//			.log().all()
+//			.extract().response().asString();
+//		JsonPath js2=new JsonPath(getResponse);
+//		String courses=js2.getString("courses");
+//		System.out.println("Fetched courses are : "+courses);
+		
+		
+		//POJO class
+				GetCourse getCourse=given()
+					.queryParam("access_token", oAuthToken)
+				.get("oauthapi/getCourseDetails")
+				.as(GetCourse.class);
+				
+				System.out.println("LinkedIn Link : "+getCourse.getLinkedIn());
+				System.out.println("Instructor name : "+getCourse.getInstructor());
+//				System.out.println("Available API course : "+getCourse.getCourses().getApi().get(0).getCourseTitle());
+//				System.out.println("Course Price: "+getCourse.getCourses().getApi().get(0).getPrice());
+				List<Api> ApiCourses= getCourse.getCourses().getApi();
+				for (Api api : ApiCourses) {
+					System.out.println("Course Title : "+api.getCourseTitle());
+					System.out.println("Price : "+api.getPrice());
+					System.out.println("-----------------");
+				}
+				
+				//Print Json
+				
+				ObjectMapper mapper=new ObjectMapper();
+				String JSONResponse=mapper.writeValueAsString(getCourse);
+				System.out.println(JSONResponse);
+				
 	}
 
 }
